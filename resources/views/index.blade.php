1d8c911f1869856235b51af88f84659c4f6e56bd
Adrian Ortiz Martinez <adrianortizmartinez@gmail.com>
2019-06-23 23:52:26 -0500

    Added contact section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -125,6 +125,42 @@
             </div>
         </section>
     </section>
+
+    <section class="analysis-question-container">
+        <p class="analysis-question-container-text-a">Brindamos información para toma de decisiones de negocios enfocados siempre en realizar las preguntas correctas al análisis en cuestión.</p>
+        <p class="analysis-question-container-text-b">Poder realizar las preguntas correctas en el contorno de la empresa podrá generar información de gran valor.</p>
+
+        <div class="analysis-question-container-block">
+            <div class="analysis-question-container-block-a">
+                <p>¿Quiénes son mis principales competidores, cuanto venden y donde están posicionados?</p>
+                <p>¿Qué potencial tiene un mercado y que oportunidades de negocio puede brindar?</p>
+                <p>¿Cuál es el Market Share de mi empresa?</p>
+            </div>
+            <div class="analysis-question-container-block-b">
+                <div>
+                    <p style='background-image: url("{{ asset('/media/app/icon-analysis-08.png') }}");'></p>
+                    <p>Minería de datos</p>
+                    <p>Big data</p>
+                    <p>Analytics</p>
+                </div>
+            </div>
+            <div class="analysis-question-container-block-c">
+                <p>¿Qué zona geográfica tiene el mejor potencial de mercado?</p>
+                <p>¿Cuáles son las mejores ubicaciones para abrir una TPV?</p>
+                <p>¿Cuál es el tamaño de mi mercado?</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="contact-us-container-full">
+        <section class="contact-us-container">
+            <div>
+                <p>¿Deseas más información?</p>
+                <p>Ponte en contacto con nostros, estamos a tus ordenes.</p>
+            </div>
+            <a href="#!" style='background-image: url("{{ asset('/media/app/icon-email-white.png') }}");'>Contáctanos</a>
+        </section>
+    </section>
 @endsection
 
 @section('extra-js')
@@ -144,7 +180,7 @@
             $('.autoplay-slider-analysis').slick({
                 arrows: false,
                 slidesToShow: 5,
-                slidesToScroll: 3,
+                slidesToScroll: 1,
                 autoplay: true,
                 autoplaySpeed: 4000,
                 draggable: true,
@@ -154,7 +190,7 @@
                         breakpoint: 1025,
                         settings: {
                             slidesToShow: 4,
-                            slidesToScroll: 4,
+                            slidesToScroll: 2,
                             infinite: true,
                             dots: true
                         }
@@ -174,7 +210,7 @@
                         }
                     },
                     {
-                        breakpoint: 376,
+                        breakpoint: 374,
                         settings: {
                             slidesToShow: 1,
                             slidesToScroll: 1
