674c540590ff3f8760775984c0f803b3f780daa6
Adrian Ortiz Martinez <adrianortizmartinez@gmail.com>
2019-06-23 21:16:11 -0500

    Added services section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
 
 
 @section('content')
-    <div class="autoplay-slider">
+    <section class="autoplay-slider">
 
         <div class="slider-item" style='background-image: url("{{ asset('/media/slider/slider-1.png') }}");'>
             <div class="slider-bg-item">
@@ -20,7 +20,7 @@
                 </div>
             </div>
         </div>
-        <div class="slider-item" style='background-image: url("{{ asset('/media/slider/slider-1.png') }}");'>
+        <div class="slider-item" style='background-image: url("{{ asset('/media/slider/slider-2.png') }}");'>
             <div class="slider-bg-item">
                 <div class="slider-info-item">
                     <h2>Especialistas en</h2>
@@ -29,7 +29,16 @@
                 </div>
             </div>
         </div>
-        <div class="slider-item" style='background-image: url("{{ asset('/media/slider/slider-1.png') }}");'>
+        <div class="slider-item" style='background-image: url("{{ asset('/media/slider/slider-3.png') }}");'>
+            <div class="slider-bg-item">
+                <div class="slider-info-item">
+                    <h2>Especialistas en</h2>
+                    <h1>Soluciones</h1>
+                    <a href="#!" class="btn-slider" style='background-image: url("{{ asset('/media/app/icon-email-white.png') }}");'>Contacto</a>
+                </div>
+            </div>
+        </div>
+        <div class="slider-item" style='background-image: url("{{ asset('/media/slider/slider-0.png') }}");'>
             <div class="slider-bg-item">
                 <div class="slider-info-item">
                     <h2>Especialistas en</h2>
@@ -39,7 +48,39 @@
             </div>
         </div>
 
-    </div>
+    </section>
+
+    <section class="services-container">
+        <div class="services-title-container">
+            <h2>Servicios</h2>
+        </div>
+
+        <div class="services-container-item" style='background-image: url("{{ asset('/media/slider/slider-0.png') }}");'>
+            <div class="services-container-item-bg">
+                <h3>Estudio de mercado</h3>
+                <a href="#!">Leer más</a>
+            </div>
+        </div>
+        <div class="services-container-item" style='background-image: url("{{ asset('/media/slider/slider-1.png') }}");'>
+            <div class="services-container-item-bg">
+                <h3>Estudio de consumidor</h3>
+                <a href="#!">Leer más</a>
+            </div>
+        </div>
+        <div class="services-container-item" style='background-image: url("{{ asset('/media/slider/slider-2.png') }}");'>
+            <div class="services-container-item-bg">
+                <h3>Validación de procesos</h3>
+                <a href="#!">Leer más</a>
+            </div>
+        </div>
+        <div class="services-container-item" style='background-image: url("{{ asset('/media/slider/slider-3.png') }}");'>
+            <div class="services-container-item-bg">
+                <h3>Gestión de riesgos</h3>
+                <a href="#!">Leer más</a>
+            </div>
+        </div>
+
+    </section>
 @endsection
 
 @section('extra-js')
